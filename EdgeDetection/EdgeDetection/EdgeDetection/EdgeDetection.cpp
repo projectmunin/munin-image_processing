a8573b3bc87a77adeb1b897f5e287e53962e5137
feha <felix@hallqvist.se>
2014-02-18 16:20:39 +0100

    A branch for EdgeDetection
    Creating an EdgeDetection Branch and a start for it.
    Fixes #6

---
+++ EdgeDetection/EdgeDetection/EdgeDetection/EdgeDetection.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+int main( void )
+{
+	
+	printf( "EdgeDetection: main started\n" );
+
+	system("pause");
+
+	return 0;
+
+}
+
